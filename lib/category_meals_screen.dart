9b9d67f3fdbe6e1a6daff241ba5ba000a8144323
Vinit Gundeti <vinit@glideinvest.com>
2023-01-03 17:40:16 +0530

    fonts added, navigation setup

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  final String categoryId;
+  final String categoryTitle;
+  const CategoryMealsScreen(this.categoryId, this.categoryTitle, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(categoryTitle)),
+      body: const Text('The meals screen'),
+    );
+  }
+}
