22096ec70701e64c39d6f51917c88530ce1f798c
Vinit Gundeti <vinit@glideinvest.com>
2023-01-04 10:42:30 +0530

    pushnamed and data manipulation

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:udemy_three_meals_app/dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  final String categoryId;
-  final String categoryTitle;
-  const CategoryMealsScreen(this.categoryId, this.categoryTitle, {super.key});
+  const CategoryMealsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context)?.settings.arguments as Map<String, String>;
+    final String? categoryId = routeArgs['id'];
+    final String? categoryTitle = routeArgs['title'];
+    // final categoryMeals = DUMMY_MEALS.where((element) {
+    //   return element.id.contains(categoryId!);
+    // },).toList();
+
+    final categoryMeals = DUMMY_MEALS
+        .where(
+          (element) => element.categories.contains(categoryId!),
+        )
+        .toList();
     return Scaffold(
-      appBar: AppBar(title: Text(categoryTitle)),
-      body: const Text('The meals screen'),
+      appBar: AppBar(title: Text(categoryTitle as String)),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:udemy_three_meals_app/categories_screen.dart';
+import 'package:udemy_three_meals_app/category_meals_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -29,6 +30,9 @@ class MyApp extends StatelessWidget {
             )),
       ),
       home: const CategoriesScreen(),
+      routes: {
+        '/category-meals':(context) => const CategoryMealsScreen(),
+      },
     );
   }
 }
